e56158c6598d34df06c23b4aa4394085d9192a29
Ream <reamtr25@gmail.com>
2022-12-17 15:29:51 +0300

    Задача №2 к семминару от 17.12.2022

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,13 +1,15 @@
-﻿int a = 5;
-int b = 7;
-int max = a;
+﻿Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
 
-if (b > max)
-{
-    max = b;
-}
-else
-{
-    max = a;
-}
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+
+
 Console.WriteLine(max);
