ff0359f3220f05bc2e6b67754ee42f2d9ec6757c
Ream <reamtr25@gmail.com>
2022-12-18 12:45:59 +0300

    Задача №6 решение

--- HomeWork6/Program.cs
+++ HomeWork6/Program.cs
@@ -1,11 +1,15 @@
 ﻿Console.WriteLine("Введите число которое проверям на четность-нечетность: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-if (number %2 == 0)
+if (number < 0)
 {
-    Console.WriteLine($"Число {number} является четным");
+    number = number * -1;
+}
+if  (number %2 == 0)
+{
+    Console.WriteLine("Число является четным");
 }
 else
 {
-    Console.WriteLine($"Число {number} является нечетным");
+    Console.WriteLine("Число не является четным");
 }
