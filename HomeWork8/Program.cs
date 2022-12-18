12783f1e35eb07a61b73b54d944fdaf2acd057e4
Ream <reamtr25@gmail.com>
2022-12-18 14:00:57 +0300

    Тело программы для Задачи №8

--- HomeWork8/Program.cs
+++ HomeWork8/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите количество чисел для проверки четный-нечетный: ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+int index = 0;
+int [] array = new int[numbers];
+while (index < numbers) // Ввод чисел в массив
+{
+    Console.WriteLine($"Введите {index+1}) число: ");
+    array[index] = Convert.ToInt32(Console.ReadLine());
+    index++;
+}
+index = 0;
+Console.WriteLine();
+Console.Write("Имеем набор чилес: ");
+while (index < numbers) // Вывод чисел массива в консоль ввиде строки
+{
+    Console.Write($"{array[index]}. ");
+    index++;
+}
+index = 0;
+int n = 0;
+Console.WriteLine();
+Console.WriteLine("Из ведденых числел, четными являются:");
+while (index < numbers)
+{
+    if (array[index] %2 == 0)
+    {
+        Console.Write($"{array[index]}. ");
+        n = n + 1;
+    }
+    index++;
+}
+if (n == 0)
+{
+    Console.Write("Ниодного");
+}
