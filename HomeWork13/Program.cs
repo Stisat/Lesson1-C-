e6e7e11e461639358de349b6f32c55a79424dad7
Ream <reamtr25@gmail.com>
2022-12-18 15:00:57 +0300

    Тело задачи №13

--- HomeWork13/Program.cs
+++ HomeWork13/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = new Random().Next(1, 100000);
+Console.WriteLine($"Компьютер число от 1 до 100000: {number}");
+string str = Convert.ToString(number);
+int lenstr = str.Length;
+// Console.WriteLine(lenstr);
+if (lenstr < 3)
+{
+    Console.WriteLine($"Третьей цифры нет");
+}
+else
+{
+    Console.WriteLine($"Третья цифра в числе - {str[2]}");
+}
