210b59fe8a34a645495eec1b79c62e38c6620941
Ream <reamtr25@gmail.com>
2022-12-18 12:50:33 +0300

    Задача №6 решение корректировка

--- HomeWork6/Program.cs
+++ HomeWork6/Program.cs
@@ -1,10 +1,6 @@
 ﻿Console.WriteLine("Введите число которое проверям на четность-нечетность: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-if (number < 0)
-{
-    number = number * -1;
-}
 if  (number %2 == 0)
 {
     Console.WriteLine("Число является четным");
