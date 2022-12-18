d35df31429b246b4d576be35c2335fcdbdeb36f0
Ream <reamtr25@gmail.com>
2022-12-18 10:16:30 +0300

    Код на две задачи сразу, Задача №2, №4

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -7,9 +7,12 @@ int num2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите третье число: ");
 int num3 = Convert.ToInt32(Console.ReadLine());
 int max = num1;
+int min = num1;
 
 if (num2 > max) max = num2;
 if (num3 > max) max = num3;
+if (num2 < min) min = num2;
+if (num3 < min) min = num3;
 
-
-Console.WriteLine(max);
+Console.WriteLine($"Минимальное число:  {min}");
+Console.WriteLine($"Максимальное число:  {max}");
