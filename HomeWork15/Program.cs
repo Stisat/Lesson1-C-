a5dde70e17fe0ec8845e15402a75bca7bae13cb4
Ream <reamtr25@gmail.com>
2022-12-18 15:24:38 +0300

    Тело задачи №15

--- HomeWork15/Program.cs
+++ HomeWork15/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите любою цифру, обозначающую день недели от 1 до 7:");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day > 7) 
+{
+Console.WriteLine("Вы ушли за пределы одной недели");
+}
+else
+{
+    if ((day == 6) || (day == 7)) // обычная проверка 7 дней в неделе.
+    {
+        Console.WriteLine("Выходной день");
+    }
+    else Console.WriteLine("Будний день");
+}
