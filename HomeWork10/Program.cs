f5f018545f497c225ea05f4b55ab90095acb541f
Ream <reamtr25@gmail.com>
2022-12-18 14:19:03 +0300

    Тело Задачи №10 семинара от 18.12.2022

--- HomeWork10/Program.cs
+++ HomeWork10/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿int number = new Random().Next(100, 1000);
+Console.WriteLine($"Сгенерировано трехзначное число: {number}");
+
+int n1 = number /100;
+int n3 = number %10;
+int n2 = (number - n1*100 -n3) /10;
+Console.WriteLine();
+Console.WriteLine($"Вторая цифра этого числа - {n2}");
