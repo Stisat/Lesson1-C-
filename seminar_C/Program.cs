7948a187c8b5d5b99b81c33dfac4d9037d2c217c
Ream <reamtr25@gmail.com>
2022-12-24 09:43:03 +0300

    Семинар от 24.12.2022

--- seminar_C/Program.cs
+++ seminar_C/Program.cs
@@ -1,10 +1,13 @@
-﻿Console.Write("Введите первое число: ");
-int numFirst = Convert.ToInt32(Console.ReadLine());
+﻿// Console.Write("Введите первое число: ");
+// int numFirst = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите второе число: ");
-int numSecond = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int numSecond = Convert.ToInt32(Console.ReadLine());
 
-if (numFirst == numSecond*numSecond)
-    Console.Write($"Число {numFirst} является квадратом {numSecond}");
-else 
-    Console.Write($"Число {numFirst} не является квадратом {numSecond}");
+// if (numFirst == numSecond*numSecond)
+//     Console.Write($"Число {numFirst} является квадратом {numSecond}");
+// else 
+//     Console.Write($"Число {numFirst} не является квадратом {numSecond}");
+
+// Семинар от 24.12.2022
+
