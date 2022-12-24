8c7732934ef59a7686012c8372feddbfeb7f1511
Ream <reamtr25@gmail.com>
2022-12-24 17:56:28 +0300

    задачи за семинар 24.12.2022_

--- seminar_C/Program.cs
+++ seminar_C/Program.cs
@@ -90,29 +90,29 @@ else
 //Напишите программу, которая принимает на вход координаты двух точек
 // и находит расстояние между ними в 2D пространстве.
 
-Console.Clear();
-Console.Write("Введите X1: ");
-int x1=int.Parse(Console.ReadLine());
-Console.Write("Введите Y1: ");
-int y1=int.Parse(Console.ReadLine());
-Console.Write("Введите X2: ");
-int x2=int.Parse(Console.ReadLine());
-Console.Write("Введите Y2: ");
-int y2=int.Parse(Console.ReadLine());
+// Console.Clear();
+// Console.Write("Введите X1: ");
+// int x1=int.Parse(Console.ReadLine());
+// Console.Write("Введите Y1: ");
+// int y1=int.Parse(Console.ReadLine());
+// Console.Write("Введите X2: ");
+// int x2=int.Parse(Console.ReadLine());
+// Console.Write("Введите Y2: ");
+// int y2=int.Parse(Console.ReadLine());
 
-double d = Math.Sqrt(Math.Pow(x1-x2, 2) + Math.Pow(y1-y2, 2));
+// double d = Math.Sqrt(Math.Pow(x1-x2, 2) + Math.Pow(y1-y2, 2));
 
-Console.WriteLine($"d={d:f5}");
+// Console.WriteLine($"d={d:f5}");
 
 
 //Напишите программу, которая принимает на вход число (N) 
 //и выдаёт таблицу квадратов чисел от 1 до N.
 
-Console.Clear();
-Console.Write("Введите число N: ");
-int N = int.Parse(Console.ReadLine());
+// Console.Clear();
+// Console.Write("Введите число N: ");
+// int N = int.Parse(Console.ReadLine());
 
-for(int i = 1; i <= N; i++)
-{
-    Console.Write($"{i*i} ");
-}
+// for(int i = 1; i <= N; i++)
+// {
+//     Console.Write($"{i*i} ");
+// }
