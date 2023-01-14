c952dbcf0695b2e507c49770fcc3721efeeeea8d
Ream <reamtr25@gmail.com>
2023-01-14 13:17:20 +0300

    Домашная задача №25

---
+++ HomeWork25/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine("Введите число А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+numberA = Math.Abs(numberA); // исключение отрицательного ввода числа А;
+Console.WriteLine("Введите число В: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+numberB = Math.Abs(numberB); // исключение отрицательного ввода числа В;
+int result = 1;
+
+for (int i = 0; i < numberB; i++)
+{
+    result = result * numberA;
+}
+
+Console.WriteLine($"Число {numberA} в степени {numberB} равняется {result}");
+
