01ed8bc25e8b729ba48e91558ea1c31974de90d6
Ream <reamtr25@gmail.com>
2023-01-15 15:23:01 +0300

    Функция перевода числа в массив цифр числа

---
+++ MyFuctions/Program.cs
@@ -0,0 +1,24 @@
+﻿// Функция занесения любого натурального числа в массив
+
+int [] BinMass (int number)
+{
+    number = Math.Abs(number);
+    string str = Convert.ToString(number);
+    int size = str.Length;
+    int [] result = new int[size];
+    int tempvar = 0;
+    for (int i = size-1; i >= 0; i--)
+    {
+        tempvar = number %10;
+        result[i] = tempvar;
+        number = number / 10;
+    }
+    return result;
+}  
+Console.Clear(); 
+int [] array = BinMass(456);
+int [] ar2 = BinMass(45632);
+Console.WriteLine($" [ {String.Join(", ", array)} ] ");
+Console.WriteLine();
+Console.WriteLine($" [ {String.Join(", ", ar2)} ] ");
+// ________________________________ 
