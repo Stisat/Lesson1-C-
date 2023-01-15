9f6eb818d8a6badb78ee67c8a983cc17bdde07fb
Ream <reamtr25@gmail.com>
2023-01-15 11:17:56 +0300

    Задача 27 изменение формулы подсчета цифр

--- HomeWork27/Program.cs
+++ HomeWork27/Program.cs
@@ -1,27 +1,24 @@
 ﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
 Console.Clear();
-Console.WriteLine("Введите число: ");
+Console.Write("Введите натуральное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 number = Math.Abs(number); // исключение отрицательного ввода числа для определения длины строки;
 string str = Convert.ToString(number);
 int size = str.Length;
 int [] numbers = new int[size];
 int sum = 0;
-int temval = 0;
+int tempvar = 0;
 int index = size;
 int printnum = number;
 while (index > 0 )  // внесение цифр из числа в массив (можно конечно не давать право пользователю не вводить число);
     {
         index = index - 1;
-        temval = number %10;
-        numbers[index] = temval;
+        tempvar = number %10;
+        numbers[index] = tempvar;
         number = number / 10;
+        sum = sum + numbers[index];
     }
 
-for (int i = 0; i < size; i++) // Нахождение суммы цифр массива;
-{
-    sum = sum + numbers[i];
-}
 Console.WriteLine();
 Console.WriteLine($"Сумма цифр числа {printnum} равна {sum}");
