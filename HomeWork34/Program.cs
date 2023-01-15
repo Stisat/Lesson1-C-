dc004120829f470dda9ef33fa6d0cb0599368e3e
Ream <reamtr25@gmail.com>
2023-01-15 13:12:33 +0300

    Задача №34 через функции

---
+++ HomeWork34/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int [] BinMass (int size) // Функция задания массива случайных трёхзначных чисел размером size;
+{
+    int [] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(100, 1000);
+    }
+    return result;
+}
+
+int NumEvenVal (int [] collect) // Функция нахождения количества четных чисел в массиве;
+{
+    int size = collect.Length;
+    int result = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (collect[i] %2 == 0)
+        {
+            result = result + 1;
+        }
+    }
+    return result;
+}
+
+Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int count = Convert.ToInt32(Console.ReadLine()); 
+int [] array = BinMass(count);
+Console.WriteLine($" [ {String.Join(", ", array)} ] ");
+Console.WriteLine();
+Console.WriteLine($"Количество четных чисел в массиве равно: {NumEvenVal(array)}");
