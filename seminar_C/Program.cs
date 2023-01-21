922781408c32d89aba43f7f1771de195b0f3d118
Ream <reamtr25@gmail.com>
2023-01-21 12:24:56 +0300

    Семинар 21.01.2023

--- seminar_C/Program.cs
+++ seminar_C/Program.cs
@@ -189,13 +189,13 @@
 
 // Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
 
-int [] arr = new int[8];
+// int [] arr = new int[8];
 
-for (int i = 0; i < 8; i++)
-{
-    arr[i] = new Random().Next(2);
-}
-Console.WriteLine($" / {String.Join(", ", arr)} / ");
+// for (int i = 0; i < 8; i++)
+// {
+//     arr[i] = new Random().Next(2);
+// }
+// Console.WriteLine($" / {String.Join(", ", arr)} / ");
 
 //вариант через функцию:
 // int[] array = GetBinaryArray(8);
@@ -212,3 +212,167 @@ Console.WriteLine($" / {String.Join(", ", arr)} / ");
 
 //     return result;
 // }
+
+
+// Семинар 21.01.2023
+
+// int [] BinMass (int size) // Функция задания массива случайных чисел от 0 до 9999 размером size;
+// {
+//     int [] result = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         result[i] = new Random().Next(0, 10000);
+//     }
+//     return result;
+// }
+
+// int [] SortArr (int [] collect) // функция сортировки массива (зеркало)
+// {
+//     int size = collect.Length;
+//     int temp = 0;
+//     for (int i = 0; i <= size /2 ; i++)
+//     {
+//         temp = collect[i];
+//         collect[i] = collect[(collect.Length -1)-i];
+//         collect[(collect.Length-1)-i] = temp;
+//     }
+//     return collect;
+// }
+// Console.Clear();
+// Console.WriteLine("Введите количестко элементов массива: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// int [] arr = BinMass(size);
+// Console.WriteLine($"[ {String.Join(", ", arr)} ] ");
+// Console.WriteLine();
+// SortArr(arr);
+// Console.WriteLine($"[ {String.Join(", ", arr)} ] ");
+
+// Задача №40 Напишите программу, которая принимает на вход три числа и проверяет,
+// может ли существовать треугольник со сторонами такой длины.
+
+// Console.WriteLine("Введите число A: ");
+// int numA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число B: ");
+// int numB = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число C: ");
+// int numC = Convert.ToInt32(Console.ReadLine());
+// if ((numA < numB + numC) && (numB < numA + numC) && (numC < numB + numA))
+// {
+//     Console.WriteLine("Треугольник может существовать");
+// }
+// else Console.WriteLine("Треугольник не может существовать");
+
+//или через массив и функции
+// int[] CreateRandomArray(int N, int start, int end) //создание массива
+// {
+//     int[] RandomArray = new int[N];
+//     for (int i = 0; i < N; i++)
+//     {
+//         RandomArray[i] = new Random().Next(start, end);
+//     }
+//     return RandomArray;
+// }
+
+// void ShowArray(int[] array) // показать массив
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+// Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// string str = String.Empty;
+// string temp = String.Empty;
+// while (num>0)
+// {
+//     temp = Convert.ToString(num%2);
+//     str = str + temp;
+//     temp = String.Empty;
+//     num = num/2;
+// }
+// int size = str.Length;
+// string strN = String.Empty;
+// for (int i = 0; i < size; i++)
+// {
+//     //Console.WriteLine(str[size-1-i]);
+//     strN = strN + str[size-1-i];
+// }
+// Console.WriteLine(strN);
+// через массив
+// void ShowArray(int[] array){
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// int[] Revers(int [] array){
+    
+//     int temp = 0;
+    
+//     for (int i=0; i < array.Length/2; i++){
+//         temp = array[i];
+//         array[i] = array[(array.Length - 1) - i];
+//         array[(array.Length - 1) - i] = temp;
+//     }
+
+//     return array;
+// }
+
+// int [] intToBin(int num){
+    
+//     int temp = num;
+//     int count = 0;
+
+//     while(Convert.ToBoolean(temp)){
+//         temp /= 2;
+//         count++;
+//     }
+
+
+//     int [] array = new int [count];
+
+//     for (int i=0; i < array.Length; i++){
+//         array[i] = num%2; 
+//         num /= 2;
+//     }
+        
+//     Revers(array);
+
+//     return array;
+// }
+
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(num);
+// int [] array = intToBin(num);
+
+// ShowArray(array);
+
+//фибоначи
+// Console.WriteLine($"Введите количество чисел Фибоначчи");
+// int val_1 = Convert.ToInt32(Console.ReadLine());
+
+// int param_1 = 0;
+// int param_2 = 1;
+// int temp = param_2;
+
+// temp = param_1 + param_2; //1
+// Console.WriteLine(param_1); //0
+// Console.WriteLine(param_2); // 1
+
+// for (int i = 0; i < val_1; i++)
+// {
+//     temp = temp + param_2; // 2
+//     param_1 = param_2;
+//     param_2 = temp - param_1;
+
+//     Console.WriteLine(temp);
+    
+// }
+
