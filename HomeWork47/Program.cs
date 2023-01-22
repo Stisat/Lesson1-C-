ba11454e6f0231e0ddaeca0a1699771e19f2e724
Ream <reamtr25@gmail.com>
2023-01-22 15:43:11 +0300

    Тело задачи №47

---
+++ HomeWork47/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+double[,] SetRealArray(int row, int column, int minValue, int maxValue)
+{
+    int RandomVal = 0;
+    double[,] result = new double[row, column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            RandomVal = new Random().Next(minValue, maxValue);
+            result[i, j] = new Random().NextDouble()*RandomVal;
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($" {inArray[i,j]:f2} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int row = 4;
+int column = 4;
+
+double [,] array =  SetRealArray(row, column, -100, 100);
+PrintArray(array);
