a0b5b8e6a8feaee40e8ebbc891765fdd02fb805b
Ream <reamtr25@gmail.com>
2023-01-29 16:54:12 +0300

    Тело Task 65

---
+++ Task65/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+void OutputNumbers(int n, int m) 
+{
+    if (n >= m)
+    {
+        OutputNumbers(n - 1,m);
+        Console.WriteLine(n);
+    }
+}
+
+Console.Write("Введите число: N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n>m)
+{
+    OutputNumbers(n, m);
+}
+if (m>n)
+{
+    OutputNumbers(m, n);
+}
+if (n == m)
+{
+    Console.WriteLine("Нет натуральных чисел");
+}
