51253f2c72d2da4e3d53c54baf51f876d8ea66cc
Ream <reamtr25@gmail.com>
2023-01-29 16:54:02 +0300

    Тело Task 63

---
+++ Task63/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 63: Задайте значение N. Напишите программу,
+// которая выведет все натуральные числа в промежутке от 1 до N.
+
+
+void OutputNumbers(int n) 
+{
+    if (n >= 1) {
+        OutputNumbers(n - 1);
+        Console.WriteLine(n);
+    }
+}
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+OutputNumbers(n);
