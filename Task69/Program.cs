db646dc3504aeed554182ced9422ab1607e21d79
Ream <reamtr25@gmail.com>
2023-01-29 16:54:24 +0300

    Тело Task 69

---
+++ Task69/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 69: 
+Console.Clear();
+Console.Write("Введите число: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите степень: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{Pow(m,n)}");
+
+int Pow(int number,int rank)
+{
+    if (rank == 0) return 1;
+    else return (number * Pow(number,rank-1));
+}
