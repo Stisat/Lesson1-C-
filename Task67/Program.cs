05c6103c7cc693102878bb5403dc6ebbdf3b7d2e
Ream <reamtr25@gmail.com>
2023-01-29 16:54:18 +0300

    Тело Task 67

---
+++ Task67/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Сумма цифр числа:"+Sum(num));
+int Sum(int inputNum)
+{
+    if (inputNum < 10) return inputNum;
+    else return inputNum % 10 + Sum(inputNum / 10);
+}
+
+
