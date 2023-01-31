591b3b4a75f312813ffe5cb63c79dc6abd7bf201
Ream <reamtr25@gmail.com>
2023-01-31 14:33:20 +0300

    Тело задачи №64

---
+++ HomeWork64/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 64: Задайте значение N. Напишите программу,
+//которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+
+void OutputNumbers(int n) 
+{
+    if (n >= 1)
+    {
+        OutputNumbers(n - 1);
+        Console.WriteLine(n);
+    }
+}
+
+Console.Write("Введите натуральное положительное число: N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+OutputNumbers(n);
